2449b385ec8a47b0122f54b094831566461c4b0a
Sherif <sherifmuhammed0@gmail.com>
2026-02-04 19:52:35 -0800

    Add browser to the BaseUrlTest

--- src/test/java/com/sherif/tests/BaseUrlTest.java
+++ src/test/java/com/sherif/tests/BaseUrlTest.java
@@ -10,9 +10,11 @@ public class BaseUrlTest {
     public void verifyBaseUrlIsLoaded() {
         String baseUrl = ConfigReader.get("base.url");
         String env = ConfigReader.get("environment");
+        String browser = ConfigReader.get("browser");
 
         System.out.println("Environment: " + env);
         System.out.println("Base URL: " + baseUrl);
+        System.out.println("Browser is: " + browser);
 
         Assert.assertNotNull(baseUrl);
     }
